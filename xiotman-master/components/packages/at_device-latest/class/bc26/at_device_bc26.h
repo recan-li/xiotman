32c212915d933f09933ae8db5604de972b3121fe
yegaorui <123@qq.com>
2023-08-03 18:17:48 +0800

    ra6m3

---
+++ xiotman-master/components/packages/at_device-latest/class/bc26/at_device_bc26.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2006-2023, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2019-12-13     qiyongzhong     first version
+ */
+
+#ifndef __AT_DEVICE_BC26_H__
+#define __AT_DEVICE_BC26_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdlib.h>
+
+#include <at_device.h>
+
+/* The maximum number of sockets supported by the bc26 device */
+#define AT_DEVICE_BC26_SOCKETS_NUM  5
+
+struct at_device_bc26
+{
+    char *device_name;
+    char *client_name;
+
+    int power_pin;
+    int power_status_pin;
+    size_t recv_line_num;
+    struct at_device device;
+
+    void *socket_data;
+    void *user_data;
+
+    rt_bool_t power_status;
+    rt_bool_t sleep_status;
+};
+
+#ifdef AT_USING_SOCKET
+
+/* bc26 device socket initialize */
+int bc26_socket_init(struct at_device *device);
+
+/* bc26 device class socket register */
+int bc26_socket_class_register(struct at_device_class *class);
+
+#endif /* AT_USING_SOCKET */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __AT_DEVICE_BC26_H__ */
+
---
+++ xiotman-master/components/packages/at_device-latest/samples/at_sample_bc28.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2006-2023, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author            Notes
+ * 2020-02-13     luhuadong         first version
+ */
+
+#include <at_device_bc28.h>
+
+#define LOG_TAG                        "at.sample.bc28"
+#include <at_log.h>
+
+#define BC28_SAMPLE_DEIVCE_NAME        "bc28"
+
+static struct at_device_bc28 _dev =
+{
+    BC28_SAMPLE_DEIVCE_NAME,
+    BC28_SAMPLE_CLIENT_NAME,
+
+    BC28_SAMPLE_POWER_PIN,
+    BC28_SAMPLE_STATUS_PIN,
+    BC28_SAMPLE_RECV_BUFF_LEN,
+};
+
+static int bc28_device_register(void)
+{
+    struct at_device_bc28 *bc28 = &_dev;
+
+    return at_device_register(&(bc28->device),
+                              bc28->device_name,
+                              bc28->client_name,
+                              AT_DEVICE_CLASS_BC28,
+                              (void *) bc28);
+}
+INIT_APP_EXPORT(bc28_device_register);
+
